a24597124b78021e8380a6b0e1e897dec57a6447
ManishaSwain7 <mainsha.swain@coforge.com>
2025-06-04 11:57:35 +0530

    Added comment to employee

--- src/main/java/com/copilotw/models/Employee.java
+++ src/main/java/com/copilotw/models/Employee.java
@@ -5,7 +5,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-
+//Employee class representing a staff member in the system
 @Entity
 @Table(name = "staff")
 public class Employee {
